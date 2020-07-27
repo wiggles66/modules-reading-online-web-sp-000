b18789fadd61a907eb1d4f41e87271c9faa1c101
Michael Waide <mvwaide@gmail.com>
2020-07-27 17:52:34 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,13 +1,4 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
-class Kid
-  extend MetaDancing
-  include Dance
-
-  attr_accessor :name
-
-  def initialize(name)
-    @name = name
-  end
-end
+v

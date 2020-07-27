7304a3947e1064f2a9d8ec0c677adab9bdbc8a3a
Michael Waide <mvwaide@gmail.com>
2020-07-27 17:59:08 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,8 +2,7 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 class Kid
-  extend FancyDance::ClassMethods
- include FancyDance::InstanceMethods
+
   extend MetaDancing
   include Dance
 
